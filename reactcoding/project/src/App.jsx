2ab8bc15ca145aa3186954a610ba502175d74453
yuna9147 <yuna220301@gmail.com>
2025-09-15 12:27:23 +0900

    .

---
+++ reactcoding/project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+ function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline text-purple-600 ring-2 ring-purple-200 rounded-lg py-3 bg-indigo-100/50">
+     Hello,React~!!
+    </h1>
+  );
+}
+
+export default App
